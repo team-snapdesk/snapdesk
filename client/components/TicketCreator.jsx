c6a97d2a8fb1017244d62268992bf1ee478899ac
Nick Primuth <nickprimuth@Nicks-MacBook-Pro.local>
2020-02-22 14:15:28 -0800

    Ticket Creator created

---
+++ client/components/TicketCreator.jsx
@@ -0,0 +1,32 @@
+/**
+ * ************************************
+* @module  TicketCreator
+* @author
+* @date
+* @description presentation component that creates a Mentee post with message, topics, and expected stars
+*
+*
+* ************************************
+*/
+
+import React from 'react';
+
+const TicketCreator = ({
+ newMessage,
+ updatedMessage,
+ expectedStars,
+ postTicket,
+}) => (
+ <div>
+   <form onSubmit={postTicket}>
+     <input
+       id="new-message"
+       value={newMessage}
+       onChange={e => updateMessage(e.target.value)}
+     />
+     <button className="primary" type="submit">Add Market</button>
+   </form>
+ </div>
+);
+
+export default MarketCreator;
