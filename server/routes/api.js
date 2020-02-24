b7f04d3ed1f21b1bfada95c4a84b9e631410c697
sean arseneault <itsmesean88@gmail.com>
2020-02-24 09:19:24 -0800

    Refactored routes to conform to AirBnB style

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
+
 const apiRouter = express.Router();
 
-
-apiRouter.get('/', 
+// ADD API ROUTES HERE
+apiRouter.get(
+  '/',
   // add middleware here
   (req, res) => res.sendStatus(200)
-)
+);
 
-// ADD API ROUTES HERE
-
-module.exports = apiRouter; 
+module.exports = apiRouter;
