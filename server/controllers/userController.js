172d495340e6ee119a5e20c583cff314de351b55
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-23 18:39:55 -0800

    added userController to handle serving user data to client

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+/**
+ * ************************************
+ *
+ * @author Joshua
+ * @date 2/23/20
+ * @description any server information associated with the user
+ *
+ * ************************************
+ */
+
+ const userController = {};
+
+ module.exports = userController;
