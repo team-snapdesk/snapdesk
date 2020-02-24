99f2360c9535f427366aced16e8aca8e7a0bb4fc
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-23 19:33:02 -0800

    added server routes for getting user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,30 @@
  *
  * ************************************
  */
+const jwt = require('jsonwebtoken');
+const jwtSecret = require('../_secret/jwtSecret');
 
- const userController = {};
+// import access to database
+const db = require('../models/userModel');
+
+const userController = {};
+
+userController.getData = (req, res, next) => {
+  // // make sure user is logged in before fetching data
+  // if (!res.locals.isLoggedIn) return next();
+  // pull github_id from fwts token
+  const { github_id } = jwt.verify(req.cookies.jwt_token, jwtSecret.secret);
+  // query data base with github id
+  const userQuery = `SELECT * FROM users WHERE github_id = ${github_id}`;
+  db.query(userQuery)
+    .then(user => {
+      console.log(user);
+      res.locals.user = user.rows[0];
+      return next();
+    })
+    .catch(err => next({ 
+      log: `Error in middleware userController.getData: ${err}` 
+    }));
+}
 
  module.exports = userController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,13 @@ const apiRouter = express.Router();
 
 // require in middleware here
 const jwtsController = require('../controllers/jwtsController');
+const userController = require('../controllers/userController');
 
 
 apiRouter.get('/user',
   jwtsController.isLoggedIn,
-  (req, res) => res.sendStatus(200)
+  userController.getData,
+  (req, res) => res.status(200).json(res.locals)
 )
 
 // ADD API ROUTES HERE
