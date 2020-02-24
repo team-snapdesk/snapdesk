b369196839211b539c0c87e6fcab042aba471964
Nick Primuth <nickprimuth@Nicks-MacBook-Pro.local>
2020-02-24 15:42:08 -0800

    Continuing to work on reducer/logic for buttons

--- client/components/MenteeTicketBox.jsx
+++ client/components/MenteeTicketBox.jsx
@@ -11,7 +11,7 @@
 
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
-
+let buttons;
 class MenteeTicketBox extends Component {
   constructor(props) {
     super(props);
@@ -24,14 +24,26 @@ class MenteeTicketBox extends Component {
       //   //your ticket has been picked up. 
       //   //Resolve is active but Delete is not
       // }
+    if (this.props.ticket.status === 'active') {
+      buttons = (
+        <span>
+          <Button disabled type="button" className="btn btn-success">Resolve</Button>
+          <Button onClick={() => this.props.deleteTicket(this.props.messageId)} type="button" className="btn btn-warning">Delete</Button>
+        </span>
+      )
+    } else {
+      buttons = (
+        <span>
+          <Button onClick={() => this.props.resolveTicket(this.props.messageId)}type="button" className="btn btn-success">Resolve</Button>
+          <Button disable type="button" className="btn btn-warning">Delete</Button>
+        </span>
+      )
+    }
     return (
     <div className="MenteeTicketBox">
       <p>Request: {this.props.messageInput}</p>
       <p>Expected Snaps: {this.props.messageRating}</p>
-      <span>
-        <button onClick={() => this.props.resolveTicket(this.props.messageId)}type="button" class="btn btn-success">Resolve</button>
-        <button onClick={() => this.props.deleteTicket(this.props.messageId)} type="button" class="btn btn-warning">Delete</button>
-      </span>
+      {buttons}
     </div>
     )
   }
