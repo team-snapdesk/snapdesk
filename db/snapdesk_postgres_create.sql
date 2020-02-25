3e849ad611f4db180737126fa012b17034e78794
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-25 15:43:33 -0800

    updated db creator file

--- db/snapdesk_postgres_create.sql
+++ db/snapdesk_postgres_create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "users" (
 	"_id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
+	"name" varchar(255),
 	"email" varchar(255),
-	"bio" TEXT NOT NULL,
+	"bio" TEXT,
 	"github_id" integer NOT NULL UNIQUE,
 	"avatar_url" varchar(255) NOT NULL UNIQUE,
 	CONSTRAINT "users_pk" PRIMARY KEY ("_id")
@@ -14,13 +14,13 @@ CREATE TABLE "users" (
 
 CREATE TABLE "tickets" (
 	"_id" serial NOT NULL,
-	"topic" varchar(255) NOT NULL,
+	"topic" varchar(255),
 	"snaps_given" integer NOT NULL,
 	"mentee_id" integer NOT NULL,
-	"mentor_id" integer NOT NULL,
+	"mentor_id" integer,
 	"status" varchar(255) NOT NULL,
 	"message" TEXT NOT NULL,
-	"feedback" TEXT NOT NULL,
+	"feedback" TEXT,
 	"timestamp" TIMESTAMP NOT NULL,
 	CONSTRAINT "tickets_pk" PRIMARY KEY ("_id")
 ) WITH (
