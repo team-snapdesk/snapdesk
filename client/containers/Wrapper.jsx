f00e8569f088f9f11e4596f8433946d854cb0470
Jessika Campbell <jessika@Jessikas-MacBook-Pro.local>
2020-02-24 17:54:31 -0800

    styled left nav bar and centered columns

--- client/containers/Wrapper.jsx
+++ client/containers/Wrapper.jsx
@@ -44,13 +44,13 @@ class Wrapper extends Component {
     return(
       <div className="wrapper">
         <div className="row align-items-start">
-          <div className="col">
+          <div className="col-2">
             <LeftNav url={this.props.userAvatar} userName={this.props.userName} />
           </div>
-          <div className="col">
+          <div className="col-8">
             <FeedContainer {...this.props} />
           </div>
-          <div className="col">
+          <div className="col-2">
             <RightNav ticketsCount={this.props.ticketsCount} />
           </div>
         </div>
