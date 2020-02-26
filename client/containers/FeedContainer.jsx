31805ee8a8e0c91e0cf86c68b7aaf2b1bcbd1775
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-26 15:32:08 -0800

    added scroll back into feed container

--- client/containers/FeedContainer.jsx
+++ client/containers/FeedContainer.jsx
@@ -86,8 +86,8 @@ class FeedContainer extends Component {
       }
 
     return (
-      <div>
-        <div className="ticketDisplay overflow-auto">
+      <div className="feed-container flex-container">
+        <div className="ticket-display overflow-auto">
           {/* map buildFeed to tickets array */}
           {/* <BystanderTicketBox /> */}
           {activeTickets}
