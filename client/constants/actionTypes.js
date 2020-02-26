05f3a7cc87d6f9111c25d519979b7dc0755d5bef
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-26 15:42:14 -0800

    added SHOW_MODAL constant

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -9,15 +9,19 @@
  * ************************************
  */
 
-// add action type constants i.e.:
+// mentee ticket actions
 export const POST_TICKET = 'POST_TICKET';
-export const ACCEPT_TICKET = 'ACCEPT_TICKET';
-export const GET_TICKETS = 'GET_TICKETS';
 export const DELETE_TICKET = 'DELETE_TICKET';
 export const RESOLVE_TICKET = 'RESOLVE_TICKET';
+// bystander ticket actions
+export const ACCEPT_TICKET = 'ACCEPT_TICKET';
 export const CANCEL_ACCEPT = 'CANCEL_ACCEPT';
+// app actions
+export const GET_TICKETS = 'GET_TICKETS';
 export const UPDATE_MESSAGE = 'UPDATE_MESSAGE';
 export const UPDATE_RATING = 'UPDATE_RATING';
+export const SHOW_MODAL = 'SHOW_MODAL';
+// user actinos
 export const USER_LOGIN = 'USER_LOGIN';
 export const LOAD_USER = 'LOAD_USER';
 export const USER_LOGOUT = 'USER_LOGOUT';
