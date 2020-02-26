9128cbe44421df12f1349eb3bce3c759d08b63ab
Dakota Gilbreath <dakotag@Dakotas-MacBook-Pro.local>
2020-02-25 19:53:07 -0800

    updated SQL Schema

--- db/snapdesk_postgres_create.sql
+++ db/snapdesk_postgres_create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE "users" (
 	"_id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
+	"name" varchar(255),
+	"active_room" integer,
 	"email" varchar(255),
-	"bio" TEXT NOT NULL,
+	"bio" TEXT,
 	"github_id" integer NOT NULL UNIQUE,
 	"avatar_url" varchar(255) NOT NULL UNIQUE,
 	CONSTRAINT "users_pk" PRIMARY KEY ("_id")
@@ -15,13 +16,13 @@ CREATE TABLE "users" (
 CREATE TABLE "tickets" (
 	"_id" serial NOT NULL,
 	"room_id" integer NOT NULL,
-	"topic" varchar(255) NOT NULL,
+	"topic" varchar(255),
 	"snaps_given" integer NOT NULL,
 	"mentee_id" integer NOT NULL,
-	"mentor_id" integer NOT NULL,
+	"mentor_id" integer,
 	"status" varchar(255) NOT NULL,
 	"message" TEXT NOT NULL,
-	"feedback" TEXT NOT NULL,
+	"feedback" TEXT,
 	"timestamp" TIMESTAMP NOT NULL,
 	CONSTRAINT "tickets_pk" PRIMARY KEY ("_id")
 ) WITH (
@@ -42,3 +43,4 @@ ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk0" FOREIGN KEY ("mentee_id") REF
 ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk1" FOREIGN KEY ("mentor_id") REFERENCES "users"("_id");
 ALTER TABLE "rooms" ADD CONSTRAINT "rooms_fk0" FOREIGN KEY ("admin_id") REFERENCES "users"("_id");
 ALTER TABLE "tickets" ADD CONSTRAINT "tickets_fk2" FOREIGN KEY ("room_id") REFERENCES "rooms"("_id");
+ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("active_room") REFERENCES "rooms"("_id");
