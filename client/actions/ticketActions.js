351d8d0fe63d3a4abc0886d433581550b816cc1b
Lance Jeffers <lancej1022@gmail.com>
2020-02-26 11:59:12 -0800

    ignore this

--- client/actions/ticketActions.js
+++ client/actions/ticketActions.js
@@ -14,91 +14,86 @@ import axios from 'axios';
 import * as types from '../constants/actionTypes';
 
 export const postTicket = () => (dispatch, getState) =>
-  axios
-    .post('/api/tickets', {
-      mentee_id: getState().user.userId,
-      message: getState().tickets.messageInput,
-      status: 'active',
-      snaps_given: getState().tickets.messageRating,
-    })
-    .then(({ data }) => {
-      if (!data.isLoggedIn) {
-        dispatch({
-          type: types.USER_LOGOUT,
-          payload: data,
+    axios
+        .post('/api/tickets', {
+            mentee_id: getState().user.userId,
+            message: getState().tickets.messageInput,
+            status: 'active',
+            snaps_given: getState().tickets.messageRating
         })
-      }
-      else {
-        dispatch({
-          type: types.POST_TICKET,
-          payload: data,
-        })
-      }     
-    })
+        .then(({ data }) => {
+            if (!data.isLoggedIn) {
+                dispatch({
+                    type: types.USER_LOGOUT,
+                    payload: data
+                });
+            } else {
+                dispatch({
+                    type: types.POST_TICKET,
+                    payload: data
+                });
+            }
+        });
 
 export const getTickets = () => dispatch =>
-  axios
-    .get('/api/tickets')
-    .then(({ data }) => {
-      if (!data.isLoggedIn) {
-        dispatch({
-          type: types.USER_LOGOUT,
-          payload: data,
-        })
-      }
-      else {
-        dispatch({
-          type: types.GET_TICKETS,
-          payload: data.activeTickets || [],
-        })
-      }     
-    })
+    axios.get('/api/tickets').then(({ data }) => {
+        if (!data.isLoggedIn) {
+            dispatch({
+                type: types.USER_LOGOUT,
+                payload: data
+            });
+        } else {
+            dispatch({
+                type: types.GET_TICKETS,
+                payload: data.activeTickets || []
+            });
+        }
+    });
 
 export const updateMessage = event => ({
-  type: types.UPDATE_MESSAGE,
-  payload: event.target.value,
+    type: types.UPDATE_MESSAGE,
+    payload: event.target.value
 });
 
 export const updateRating = event => ({
-  type: types.UPDATE_RATING,
-  payload: event.target.value,
+    type: types.UPDATE_RATING,
+    payload: event.target.value
 });
 
 export const deleteTicket = id => (dispatch, getState) =>
-  axios
-    .put('/api/tickets/delete', {
-      ticketId: id,
-      status: 'deleted',
-    })
-    .then(({ data }) => {
-      if (!data.isLoggedIn) {
-        dispatch({
-          type: types.USER_LOGOUT,
-          payload: data,
+    axios
+        .put('/api/tickets/delete', {
+            ticketId: id,
+            status: 'deleted'
         })
-      }
-      else {
-        dispatch({
-          type: types.DELETE_TICKET,
-          payload: id,
-        })
-      }     
-    })
+        .then(({ data }) => {
+            if (!data.isLoggedIn) {
+                dispatch({
+                    type: types.USER_LOGOUT,
+                    payload: data
+                });
+            } else {
+                dispatch({
+                    type: types.DELETE_TICKET,
+                    payload: id
+                });
+            }
+        });
 
 export const resolveTicket = id => ({
-  type: types.RESOLVE_TICKET,
-  payload: id,
-})
+    type: types.RESOLVE_TICKET,
+    payload: id
+});
 
 export const acceptTicket = id => ({
-  type: types.ACCEPT_TICKET,
-  payload: id,
-})
+    type: types.ACCEPT_TICKET,
+    payload: id
+});
 
 export const cancelAccept = id => ({
-  type: types.CANCEL_ACCEPT,
-  payload: id,
-})
+    type: types.CANCEL_ACCEPT,
+    payload: id
+});
 
 // export const acceptTicket = event => (dispatch, getState) => {
 //   event.preventDefault();
