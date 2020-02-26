685eb3c5cda1a84de8f4af14f8afbdf2faa23986
Lance Jeffers <lancej1022@gmail.com>
2020-02-26 11:42:20 -0800

    created a working resolve button -- waiting on endpoint to be finished

--- client/containers/FeedContainer.jsx
+++ client/containers/FeedContainer.jsx
@@ -19,97 +19,107 @@ import TicketCreator from '../components/TicketCreator';
 // import { render } from 'node-sass';
 
 const mapStateToProps = state => ({
-  userId: state.user.userId,
-  messageInput: state.tickets.messageInput,
-  messageRating: state.tickets.messageRating,
-  activeTickets: state.tickets.activeTickets,
-  messageRating: state.tickets.messageRating,
-  ticketsCount: state.tickets.ticketsCount,
+    userId: state.user.userId,
+    messageInput: state.tickets.messageInput,
+    messageRating: state.tickets.messageRating,
+    activeTickets: state.tickets.activeTickets,
+    ticketsCount: state.tickets.ticketsCount
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 class FeedContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  componentWillMount() {
-    this.props.getTickets();
-  }
+    componentWillMount() {
+        this.props.getTickets();
+    }
 
-  componentDidMount() {
-    this.interval = setInterval(() => this.props.getTickets(), 5000);
-  }
+    componentDidMount() {
+        this.interval = setInterval(() => this.props.getTickets(), 5000);
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-    document.title = 'SnapDesk';
-  }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+        document.title = 'SnapDesk';
+    }
 
-  componentDidUpdate() {
-    document.title = '(' + this.props.ticketsCount + ') ' + 'SnapDesk';
-  }
+    componentDidUpdate() {
+        document.title = `(${this.props.ticketsCount}) ` + `SnapDesk`;
+    }
 
-  render() {
-    // if there are no active tickets, display a message in the background saying nothing here
-    // do not render it when a ticket is added
+    render() {
+        // if there are no active tickets, display a message in the background saying nothing here
+        // do not render it when a ticket is added
 
-    // build activeTickets list
-    // later add conditionals to check which box should be rendered based on the posterId vs logged in user
-    let activeTickets;
-    // console.log('ACTIVE TICKETS: ', this.props.activeTickets);
-    if (!this.props.activeTickets || this.props.activeTickets.length === 0) {
-      activeTickets = <p>No active tickets</p>;
-    } else {
-      activeTickets = [];
-      for (let i = 0; i < this.props.activeTickets.length; i++) {
-        let ticketBox;
-        if (this.props.userId !== this.props.activeTickets[i].menteeId) {
-          //ticket should render bystanderticketbox
-          ticketBox = (
-            <BystanderTicketBox 
-            cancelAccept={this.props.cancelAccept}
-            acceptTicket={this.props.acceptTicket}
-            messageInput={this.props.activeTickets[i].messageInput}
-            messageRating={this.props.activeTickets[i].messageRating}
-            ticket={this.props.activeTickets[i]}
-            key={this.props.activeTickets[i].messageId}
-            />
-            )
-          } else {
-            ticketBox = (
-              <MenteeTicketBox
-              deleteTicket={this.props.deleteTicket}
-              resolveTicket={this.props.resolveTicket}
-              messageInput={this.props.activeTickets[i].messageInput}
-              messageRating={this.props.activeTickets[i].messageRating}
-              ticket={this.props.activeTickets[i]}
-              key={this.props.activeTickets[i].messageId}
-              />
-              )
-          }
-          
-          activeTickets.push(ticketBox);
+        // build activeTickets list
+        // later add conditionals to check which box should be rendered based on the posterId vs logged in user
+        let activeTickets;
+        // console.log('ACTIVE TICKETS: ', this.props.activeTickets);
+        if (
+            !this.props.activeTickets ||
+            this.props.activeTickets.length === 0
+        ) {
+            activeTickets = <p>No active tickets</p>;
+        } else {
+            // build out an array of all tickets and track who created eacg ticket so we can properly accept vs resolve them
+            activeTickets = [];
+            for (let i = 0; i < this.props.activeTickets.length; i++) {
+                let ticketBox;
+                if (
+                    this.props.userId !== this.props.activeTickets[i].menteeId
+                ) {
+                    //ticket should render bystanderticketbox
+                    ticketBox = (
+                        <BystanderTicketBox
+                            cancelAccept={this.props.cancelAccept}
+                            acceptTicket={this.props.acceptTicket}
+                            messageInput={
+                                this.props.activeTickets[i].messageInput
+                            }
+                            messageRating={
+                                this.props.activeTickets[i].messageRating
+                            }
+                            ticket={this.props.activeTickets[i]}
+                            key={this.props.activeTickets[i].messageId}
+                        />
+                    );
+                } else {
+                    ticketBox = (
+                        <MenteeTicketBox
+                            deleteTicket={this.props.deleteTicket}
+                            resolveTicket={this.props.resolveTicket}
+                            messageInput={
+                                this.props.activeTickets[i].messageInput
+                            }
+                            messageRating={
+                                this.props.activeTickets[i].messageRating
+                            }
+                            ticket={this.props.activeTickets[i]}
+                            key={this.props.activeTickets[i].messageId}
+                        />
+                    );
+                }
+
+                activeTickets.push(ticketBox);
+            }
         }
-      }
 
-    return (
-      <div>
-        <div className="ticketDisplay overflow-auto">
-          {/* map buildFeed to tickets array */}
-          {/* <BystanderTicketBox /> */}
-          {activeTickets}
-        </div>
-        <div className="ticketCreator">
-          <TicketCreator {...this.props} key={this.props.userId} />
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <div className="ticketDisplay overflow-auto">
+                    {/* map buildFeed to tickets array */}
+                    {/* <BystanderTicketBox /> */}
+                    {activeTickets}
+                </div>
+                <div className="ticketCreator">
+                    <TicketCreator {...this.props} key={this.props.userId} />
+                </div>
+            </div>
+        );
+    }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FeedContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(FeedContainer);
--- client/index.js
+++ client/index.js
@@ -12,15 +12,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
+import App from './App';
 import store from './store';
 import styles from './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(
-  // wrap the App in the Provider and pass in the store
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('contents')
+    // wrap the App in the Provider and pass in the store
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('contents')
 );
