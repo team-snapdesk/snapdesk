c986b6350c12443bdb810a7f48fbf2b40bfbcdc1
MrAustin2u <aaustin86@gmail.com>
2020-02-26 21:12:24 -0800

    Created conditional logic for whether in dev or production mode to redirect to localhost:3000 or localhost:8080

--- server/routes/login.js
+++ server/routes/login.js
@@ -11,29 +11,33 @@ const githubSecret = require('../_secret/githubSecret');
 
 // first step of github oauth, direct user to github login page
 loginRouter.get('/oauth', (req, res) => {
-  const url =
-    'https://github.com/login/oauth/authorize?' +
-    'scope=user&' +
-    'redirect_uri=http://localhost:3000/login/callback&' +
-    `client_id=${githubSecret.clientId}`;
+    const url =
+        'https://github.com/login/oauth/authorize?' +
+        'scope=user&' +
+        'redirect_uri=http://localhost:3000/login/callback&' +
+        `client_id=${githubSecret.clientId}`;
 
-  return res.redirect(url);
+    return res.redirect(url);
 });
 
 // redirect from github including the temporary code from user
 loginRouter.get(
-  '/callback',
-  githubController.token,
-  githubController.userData,
-  githubController.createUser,
-  jwtsController.loginUser,
-  (req, res) => {
-    res.redirect('/');
-  }
+    '/callback',
+    githubController.token,
+    githubController.userData,
+    githubController.createUser,
+    jwtsController.loginUser,
+    (req, res) => {
+        if (process.env.NODE_ENV === 'production') {
+            res.redirect('/');
+        } else {
+            res.redirect('http://localhost:8080');
+        }
+    }
 );
 
 loginRouter.get('/verify', jwtsController.isLoggedIn, (req, res) => {
-  res.status(200).json(res.locals);
+    res.status(200).json(res.locals);
 });
 
 module.exports = loginRouter;
--- server/server.js
+++ server/server.js
@@ -32,14 +32,15 @@ app.use('/api', apiRouter);
 app.use('/login', loginRouter);
 
 // handle static files
-app.use('/build', express.static(path.join(__dirname, '../build')));
+
 app.use(express.static(path.join(__dirname, '../img')));
 
 // response with main app
 if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) =>
-    res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
-  );
+    app.use('/build', express.static(path.join(__dirname, '../build')));
+    app.get('/', (req, res) =>
+        res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
+    );
 }
 
 // catch-all route handler for any requests to an unknown route
@@ -50,19 +51,20 @@ app.use((req, res) => res.sendStatus(404));
  * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
  */
 app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred. Check server logs for detials.' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 400,
+        message: { err: 'An error occurred. Check server logs for detials.' }
+    };
+    const errorObj = { ...defaultErr, ...err };
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
 });
 
 /**
  * Start server
  */
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
+    console.log(`Server is listening on port: ${PORT}`);
 });
