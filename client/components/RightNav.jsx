e8ed11a64b63922c8c2b1b4cf5b9d68b21c8f152
MrAustin2u <aaustin86@gmail.com>
2020-02-26 17:31:56 -0800

    Created middleware and server routes for organization tickets.

--- client/components/RightNav.jsx
+++ client/components/RightNav.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 
 const RightNav = props => (
-  <Nav defaultActiveKey="/feed" className="flex-column justify-content-end">
-    <Nav.Item>Active Tickets: {props.ticketsCount}</Nav.Item>
-    <Nav.Item>Leaderboard: </Nav.Item>
-  </Nav>
+    <Nav defaultActiveKey="/feed" className="flex-column justify-content-end">
+        <Nav.Item>Active Tickets: {props.ticketsCount}</Nav.Item>
+        <Nav.Item>Leaderboard: </Nav.Item>
+    </Nav>
 );
 
 export default RightNav;
